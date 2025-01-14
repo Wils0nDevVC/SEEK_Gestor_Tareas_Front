ca1a460e9ecfc6e3589e9d2823e11c33920bc6db
Wils0nDevVC <wilsonvc.developer@gmail.com>
2025-01-13 20:40:41 -0500

    ajuste lis

--- src/app/tarea/pages/listar-tareas/listar-tareas.component.ts
+++ src/app/tarea/pages/listar-tareas/listar-tareas.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
-import { Product, ProductResponse } from 'src/app/core/interfaces/product.interface';
+import {
+  Product,
+  ProductResponse,
+} from 'src/app/core/interfaces/product.interface';
 import { ProductService } from 'src/app/shared/services/product.service';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
@@ -15,10 +18,9 @@ import { MessageControlService } from 'src/app/shared/services/message-control.s
 @Component({
   selector: 'app-listar-tareas',
   templateUrl: './listar-tareas.component.html',
-  styleUrls: ['./listar-tareas.component.scss']
+  styleUrls: ['./listar-tareas.component.scss'],
 })
 export class ListarTareasComponent implements OnInit {
-
   faTrash = faTrash;
   txtDescription!: FormControl;
 
@@ -27,77 +29,64 @@ export class ListarTareasComponent implements OnInit {
   error: string | null = null;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
-   @ViewChild(MatSort) sort!: MatSort;
-   @ViewChild(MatTable) table!: MatTable<Product>;
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatTable) table!: MatTable<Product>;
 
-   isActive : boolean = false
+  isActive: boolean = false;
 
-   displayedColumns: string[] = [
+  displayedColumns: string[] = [
     'Nombre',
     'Descripción',
     'Precio',
-    'Cantidad de Inventario'
+    'Cantidad de Inventario',
   ];
   dataSource!: MatTableDataSource<Product>;
   constructor(
     private dialog: MatDialog,
     private productService: ProductService,
-    private messageControlService : MessageControlService
+    private messageControlService: MessageControlService
   ) {}
 
   ngOnInit(): void {
     this.getProducts();
   }
 
-
-  getProducts = ()=> {
-    this.productService.getProducts()
-    .subscribe({
-      next:(response: any)=>{
-       this.productos = response?.Data
-
-       this.dataSource = new MatTableDataSource(this.productos);
-      this.dataSource.paginator = this.paginator;
-      this.dataSource.sort = this.sort;
-
-       console.log( this.productos)
+  getProducts = () => {
+    this.productService.getProducts().subscribe({
+      next: (response: any) => {
+        this.productos = response?.Data;
+        this.dataSource = new MatTableDataSource(this.productos);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
       },
-      complete : ()=> {
-        console.log('completado')
-      },
-      error: ()=> console.error('Ocurrio un error')
-    })
-  }
+      complete: () => {},
+      error: () => console.error('Ocurrio un error'),
+    });
+  };
 
   onUpdate(product: any): void {
     // Aquí iría la lógica para actualizar el producto
   }
 
   onDeleted(product: Product): void {
-    
     const dialogRef = this.dialog.open(DialogResponseComponent, {
       disableClose: true,
-      data : {...product, message:'Desea eliminar el Producto'},
+      data: { ...product, message: 'Desea eliminar el Producto' },
       width: '250px',
     });
 
-    dialogRef.afterClosed().subscribe(r => {
+    dialogRef.afterClosed().subscribe((r) => {
       if (r) {
-
         this.productService.deleteProduct(product).subscribe({
-          next : (response)=>{
+          next: (response) => {},
+          complete: () => {
+            this.getProducts();
+            this.messageControlService.ShowSuccess('Se elimino Correctamente');
           },
-          complete: ()=>{
-            this.getProducts()
-            this.messageControlService.ShowSuccess('Se elimino Correctamente')
-          },
-          error: ()=> this.messageControlService.ShowError('Ocurrio un error')
-        })
+          error: () => this.messageControlService.ShowError('Ocurrio un error'),
+        });
       }
     });
-
   }
-  onSendNotifications(){
-
-  }
+  onSendNotifications() {}
 }
